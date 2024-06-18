f83dc944f556d917e38e79d872c34358e73d4233
Christopher <cquirozmendivel@gmail.com>
2024-06-18 14:12:54 +0200

    Cards: mobile first UP

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-form',
+  selector: 'form',
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
