f1331432d68b95b0590de702f3f0843d8167f1df
Christopher <cquirozmendivel@gmail.com>
2024-06-24 15:10:50 +0200

    Interaction form v1 UP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { FormComponent } from './form/form.component';
@@ -14,6 +14,7 @@ import { FormComponent } from './form/form.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const card = {
+  name: '',
+  number: 0
+}
 
 @Component({
-  selector: 'form',
+  selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  
+  public myForm: FormGroup = this.fb.group( {
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    number: ['', [
+      Validators.required,
+      Validators.pattern('^[0-9]*$'), // Only number
+      Validators.minLength(16) ]], // Min  16 characters
+  });
 
+  public showForm: boolean = true;
+  public showThankYou: boolean = false;
+
+  constructor( private fb: FormBuilder ){}
+
+  ngOnInit(): void {
+    //this.myForm.reset(card)
+  }
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  } 
+
+  getFieldError( field: string ): string | null {
+    const control = this.myForm.get(field);
+
+    if (!control || !control.errors) return null;
+
+    const errors = control.errors;
+
+    for(const key of Object.keys(errors)) {
+      switch( key ) {
+        case 'required':
+          return 'This field is required';
+        
+        case 'minlength':
+          return `Minimum  ${ errors['minlength'].requiredLength } characters.`
+
+        case 'pattern':
+          if (errors['pattern']?.requiredPattern === '^[0-9]*$') {
+            return 'Only numbers are allowed';
+          } else if (errors['pattern']?.requiredPattern === '^[0-9]{16}$') {
+            return 'The field must be exactly 16 digits';
+          }
+      }
+    }
+    return null;
+  }
+
+  onSave(): void {
+
+    if( this.myForm.invalid) return;
+    //console.log(this.myForm.value);  
+    this.showForm = false;
+    this.showThankYou = true;
+    
+    this.myForm.reset({});
+
+  }
 }
